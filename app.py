2391a2c16e62aaf2b837d81afe53e5f52d738e6d
Alok Kumar <56478877+Alok5102R@users.noreply.github.com>
2024-03-19 21:59:12 +0530

    Flask app created:
    - Initiated first route

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return Response("<h1>Automated Sales Forecast Generator</h1>", status=200)
+
+if __name__== '__main__':
+    app.run(debug=True)
